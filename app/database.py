267a7cb45b7c135ed779c506d6ebfd3c8ca0b218
Lucky <lucky.k1405@gmail.com>
2026-07-24 19:39:00 +0530

    Stage 4: docker-compose the whole stack

--- app/database.py
+++ app/database.py
@@ -1,4 +1,5 @@
 import os
+import time
 import psycopg
 from psycopg.rows import dict_row
 from dotenv import load_dotenv
@@ -7,11 +8,23 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
+# def get_connection():
+#     return psycopg.connect(
+#         DATABASE_URL,
+#         row_factory=dict_row
+#     )
+
 def get_connection():
-    return psycopg.connect(
-        DATABASE_URL,
-        row_factory=dict_row
-    )
+    for _ in range(10):
+        try:
+            return psycopg.connect(
+                DATABASE_URL,
+                row_factory=dict_row
+            )
+        except psycopg.OperationalError:
+            time.sleep(2)
+
+    raise Exception("Could not connect to PostgreSQL")
 
 def initialize_database():
     conn = get_connection()
